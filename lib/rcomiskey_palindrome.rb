f8efb2c9eafd6c641f2707ea3aca92eb05e545a4
Richard Comiskey <richardcomiskey@Richards-MacBook.local>
2022-01-12 11:25:23 +0000

    Finish working and refactored palindrome method

--- lib/rcomiskey_palindrome.rb
+++ lib/rcomiskey_palindrome.rb
@@ -2,7 +2,18 @@
 
 require_relative "rcomiskey_palindrome/version"
 
-module RcomiskeyPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
+
 end
